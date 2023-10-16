4d51664f64ab24fecba1f0f9de4adc8557d92bb3
Dewald Els <dewaldifels@gmail.com>
2023-10-16 14:16:49 +0200

    feat: Simplify country code management

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import PhoneNumberInput from "./components/PhoneNumberInput";
 import "./App.css";
 import { useState } from "react";
 import { PhoneNumberInputSize } from "./components/types";
+import { CountryCode } from "libphonenumber-js";
 
 const App: React.FC = () => {
-  const [countryCode, setCountryCode] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+  const [countryCode, setCountryCode] = useState<CountryCode>("ZA");
+  const [phoneNumber, setPhoneNumber] = useState("+27714834857");
 
   console.log("countryCode", countryCode);
   console.log("phoneNumber", phoneNumber);
@@ -14,8 +15,8 @@ const App: React.FC = () => {
     <div className="App">
       <PhoneNumberInput
         size={PhoneNumberInputSize.Medium}
-        defaultValue="+27714834857"
-        defaultCountryCode="ZA"
+        defaultValue={phoneNumber}
+        defaultCountryCode={countryCode}
         onCountryCodeChange={(newCountryCode) => {
           setCountryCode(newCountryCode);
         }}
